939abf1507456a3290600f34de365a9d1213f08c
Will <anobscurereference@gmail.com>
2021-02-28 11:28:46 -0500

    Band name generator

---
+++ day001.py
@@ -0,0 +1,6 @@
+print ("Welcome to the band name generator!")
+print ("\n")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+band_name = city + " " + pet
+print ("Your band name could be " + band_name + "!")
