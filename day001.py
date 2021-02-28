4c664dd165293a308dc04ad83e16e3434582b331
Will <anobscurereference@gmail.com>
2021-02-28 11:58:34 -0500

    Replace final string with f-string

--- day001.py
+++ day001.py
@@ -3,4 +3,4 @@ print ("\n")
 city = input("Which city did you grow up in?\n")
 pet = input("What is the name of a pet?\n")
 band_name = city + " " + pet
-print ("Your band name could be " + band_name + "!")
+print (f"Your band name could be {band_name}!")
