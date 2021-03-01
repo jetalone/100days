0e6b99c700f80e872aff604c07a00954be34d8c8
Will <anobscurereference@gmail.com>
2021-03-01 08:03:09 -0500

    Tip Calculator

---
+++ day002.py
@@ -0,0 +1,8 @@
+print ("Welcome to the tip calculator\n")
+total = float(input ("What was the total bill? $"))
+percentage = int(input ("What percentage tip would you like to give? 10, 12, or 15? "))
+percentage /= 100
+num = int(input ("How many people to split the bill? "))
+result = round((total + (total * percentage))/num, 2)
+
+print (f"Each person should pay: ${result}") 
