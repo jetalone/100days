8656d60f9c002590c1b53620e3e1344f8ec50cae
Will <anobscurereference@gmail.com>
2021-03-01 08:12:47 -0500

    format total

--- day002.py
+++ day002.py
@@ -4,5 +4,6 @@ percentage = int(input ("What percentage tip would you like to give? 10, 12, or 
 percentage /= 100
 num = int(input ("How many people to split the bill? "))
 result = round((total + (total * percentage))/num, 2)
+result = "{:.2f}".format(result)
 
 print (f"Each person should pay: ${result}") 
