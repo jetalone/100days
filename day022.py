ec1924e4a1d65daa28d2f132ade48545079eacaf
Will <anobscurereference@gmail.com>
2021-09-22 08:45:43 -0400

    Day 022 complete

--- day022.py
+++ day022.py
@@ -1,1 +1,52 @@
-#Day 022 Pong
+from turtle import Screen, Turtle
+from day022_paddle import Paddle
+from day022_ball import Ball
+import time
+from day022_scoreboard import Scoreboard
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = Screen()
+screen.bgcolor("Black")
+screen.setup(width = WIDTH, height = HEIGHT)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # wall collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # paddle collision
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+screen.exitonclick()
