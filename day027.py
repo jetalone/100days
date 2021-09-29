f51ff1a2d00837ce911e820a30c96220acb9c087
Will <anobscurereference@gmail.com>
2021-09-29 12:19:30 -0400

    Day 027 complete

--- day027.py
+++ day027.py
@@ -1,11 +1,35 @@
 import tkinter
 
 window = tkinter.Tk()
-window.minsize(500,300)
+window.title("Miles to Kilometers")
+
+def click():
+    mylabel3.config(text = int(myinput.get()) * 1.609)
 
 
-mybutton = tkinter.Button(text = "I am a button")
-mybutton.pack()
+myinput = tkinter.Entry(width=5)
+myinput.grid(column=1,row=0)
+
+mylabel1 = tkinter.Label(text="Miles")
+mylabel1.grid(column=2,row=0)
+
+mylabel2 = tkinter.Label(text="is equal to")
+mylabel2.grid(column=0,row=1)
+
+mylabel3 = tkinter.Label(text=0)
+mylabel3.grid(column=1,row=1)
+
+mylabel4 = tkinter.Label(text="km")
+mylabel4.grid(column=2,row=1)
+
+mybutton1 = tkinter.Button(text = "Calculate", command=click)
+mybutton1.grid(column=1,row=2)
+
+
+
+
 
 
 window.mainloop()
+
+
