2c0548c5ba622946ef4052020411f45ef4c99e2d
Will <anobscurereference@gmail.com>
2021-11-02 06:25:27 -0400

    Day 032 complete

--- day032.py
+++ day032.py
@@ -1,1 +1,10 @@
-...
+import smtplib
+
+my_email = "anobscurereference@gmail.com"
+password = "5money$%^"
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="willxmelton@gmail.com", msg="Hello")
+connection.close()
