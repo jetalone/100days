5e93e2c2331d93d09648fba2a34f854bb45b35a3
Will <anobscurereference@gmail.com>
2021-11-04 07:32:02 -0400

    Day 033 complete

--- day033.py
+++ day033.py
@@ -1,1 +1,14 @@
-...
+import requests
+
+MYLAT = 33.447750
+MYLNG = -84.455130
+
+parameters = {
+    "lat": MYLAT,
+    "lng": MYLNG
+}
+
+response = requests.get(url="http://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data)
